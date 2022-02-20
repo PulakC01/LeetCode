1596de5ad1fd1fd07607d5b8ad673534ac903175
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-02-20 16:17:59 +0530

    Time: 50 ms (19.74%), Space: 28.2 MB (12.03%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -5,28 +5,27 @@ public:
             return s;
         }
         vector<vector<char>> arr(numRows);
-        int row=0,count=0;
-        bool add = true;
+        int k = 0,t= numRows-2;
+        int n = numRows;
+       //ool add = true;
         for(int i=0;i<s.length();i++) {
-            arr[row].push_back(s.at(i));
-            if(count+1==numRows) {
-                count=0;
-                if(add) 
-                    add = false;
-                else 
-                    add = true;
+            if(k<n){
+                arr[k].push_back(s.at(i));
+                k++;
             }
-            if(add)
-                row++;
-            else
-                row--;
-            count++;
+            else if(t>0){
+               arr[t].push_back(s.at(i));
+               t--;
+            }
+            if(k==n && t==0) {
+               k=0;
+               t=n-2;
+            }
         }
         string str = "";
         for(int i=0;i<numRows;i++) {
             str = str + string(arr[i].begin(), arr[i].end());
         }
-        return str;
-        
+        return str;   
     }
 };
