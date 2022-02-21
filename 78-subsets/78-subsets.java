ab7c2c5f290c4d77caea324bc75b1d8fc89170b8
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-02-21 22:06:49 +0530

    Time: 1 ms (80.14%), Space: 42.9 MB (43.78%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<Integer>());
+        
+       // Arrays.sort(nums);
+        for(int i : nums) {
+            List<List<Integer>> tmp = new ArrayList<>();
+            for(List<Integer> sub : ans) {
+                List<Integer> a = new ArrayList<>(sub);
+                a.add(i);
+                tmp.add(a);
+            }
+            ans.addAll(tmp);
+        }
+        return ans;
+    }
+}
