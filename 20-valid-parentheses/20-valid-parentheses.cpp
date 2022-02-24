af0891f55e1be85a21463e37635799e24f4ccbfb
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-02-24 15:00:53 +0530

    Time: 6 ms (11.52%), Space: 6.2 MB (79.88%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> t;
+        for(char c : s) {
+            if(c == '('|| c == '{' || c == '[') {
+                t.push(c);
+            }
+            else {
+                if(t.empty())
+                    return false;
+            
+                if(c == ')' && t.top() != '(')
+                    return false;
+                
+                if(c == '}' && t.top() != '{')
+                    return false;
+                
+                if(c == ']' && t.top() != '[')
+                    return false;
+                t.pop();
+            }
+        }
+        return t.empty();
+    }
+};
