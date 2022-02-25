d3ad2764032ffd606e2959af2393fc24bc491dd5
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-02-25 14:13:50 +0530

    Time: 60 ms (11.13%), Space: 13.9 MB (92.33%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        a = list(map(int,version1.split(".")))
+        b = list(map(int,version2.split(".")))
+        n1 = len(a)
+        n2 = len(b)
+        
+        m = max(n1,n2)
+        a += [0]*(m-n1)
+        b += [0]*(m-n2)
+        
+        for i in range(m):
+            if(a[i]>b[i]):
+                return 1
+            elif(a[i]<b[i]):
+                return -1
+            
+        return 0
