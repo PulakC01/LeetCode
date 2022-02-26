3eb7d49a4f076208ebc9ddb63714b418d41fd458
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-02-26 14:46:06 +0530

    Time: 92 ms (69.65%), Space: 31.9 MB (13.66%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0) return 0;
         set<int> s(nums.begin(), nums.end());
         nums.assign(s.begin(), s.end());
-        if(nums.size()==0) return 0;
         int longest = 0, count = 0;
         for(int i=0;i<nums.size()-1;i++) {
             if(nums[i]+1==nums[i+1]) {
                 count+=1;
                 longest = max(count,longest);
-                cout<<count<<" ";
             }
             else {
                 count=0;
