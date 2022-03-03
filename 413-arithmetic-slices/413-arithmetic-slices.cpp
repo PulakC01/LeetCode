ba9d30b397218615c0d92d5d8eb7be3379933716
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-03-03 11:51:21 +0530

    Time: 3 ms (66.63%), Space: 7.3 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -5,10 +5,9 @@ public:
         if(n<3)
             return 0;
         int count = 0, diff;
-        
-        for(int i = 0; i<nums.size()-2; ++i) {
+        for(int i = 0; i<nums.size()-2; i++) {
             diff = nums[i+1] - nums[i];
-            for(int j = i+2; j<nums.size(); ++j)  {
+            for(int j = i+2; j<nums.size(); j++) {
                 if(nums[j] - nums[j-1] == diff)
                     count++;
                 else
