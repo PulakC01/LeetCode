5ce67c93f382e7df461e33e06b50a7501c0202c6
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-03-04 08:22:08 +0530

    Time: 4 ms (92.15%), Space: 14.2 MB (49.87%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
         if(poured == 0) return 0;
-        if(poured == 1) {
-            if(query_row==0) return 1;
-            else return 0;
-        }
+        
         vector<vector<double>> dp(query_row + 1, vector<double>(query_row + 1, 0));
         dp[0][0] = poured;
         for(int row = 0; row < query_row; row++) {
@@ -17,7 +14,7 @@ public:
                 }
             }
         }
-        return min(1.0, dp[query_row][query_glass]);
+        return std::min(1.0, dp[query_row][query_glass]);
         
         
     }
