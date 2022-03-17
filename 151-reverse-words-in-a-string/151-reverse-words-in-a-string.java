c3ba0bc25c8037273a53e3792372828024f0ac95
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-03-17 21:05:45 +0530

    Time: 31 ms (8.57%), Space: 54.9 MB (5.41%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        String[] arr = s.split("\\s+");
+        int n = arr.length;
+        
+        System.out.println(n);
+        String ans = "";
+        for(int i=n-1;i>=0;i--) {
+            if(i==0)
+                ans = ans + arr[i];
+            else 
+                ans = ans + arr[i]+ " ";
+        }
+        return ans;
+    }
+}
