331b452acd6890085da69f309440878e6d06b317
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-03-19 11:59:04 +0530

    Time: 30 ms (52.05%), Space: 49 MB (50.87%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -4,9 +4,7 @@ class Solution {
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             if (set.contains(s.charAt(i))) {
-                if (map.get(s.charAt(i)) != null) {
-                    map.remove(s.charAt(i));
-                }
+                map.remove(s.charAt(i));
             } else {
                 map.put(s.charAt(i), i);
                 set.add(s.charAt(i));
