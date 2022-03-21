b3324e5986b8b2312764efbf585cee6ba642eb2f
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-03-21 15:58:04 +0530

    Time: 5 ms (71.86%), Space: 43.3 MB (15.35%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        if(s == null || s.length() == 0){
+            return null;
+        }
+        List<Integer> list = new ArrayList<>();
+        int[] arr = new int[26];  
+
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a'] = i;
+        }
+        
+        int currentMax = 0;
+        int start = 0;
+        for(int i=0;i<s.length();i++) {
+            currentMax = Math.max(currentMax , arr[s.charAt(i)-'a']);
+            if(currentMax == i) {
+                list.add(currentMax - start + 1);
+                start = currentMax + 1;
+            }
+        }
+        return list;
+        
+    }
+}
