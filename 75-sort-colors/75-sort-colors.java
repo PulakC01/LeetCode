f179f90e0a7aacf4049c51ad0dbfcd0b4fe9d194
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-03-24 21:10:37 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (78.81%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void sortColors(int[] nums) {
+     mergeSort(nums, 0, nums.length - 1);
+    }
+    public static void merge(int array[], int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        int L[] = new int[n1];
+        int M[] = new int[n2];
+        for (int i = 0; i < n1; i++)
+          L[i] = array[p + i];
+        for (int j = 0; j < n2; j++)
+          M[j] = array[q + 1 + j];
+        int i, j, k;
+        i = 0;
+        j = 0;
+        k = p;
+        while (i < n1 && j < n2) {
+          if (L[i] <= M[j]) {
+            array[k] = L[i];
+            i++;
+          } else {
+            array[k] = M[j];
+            j++;
+          }
+          k++;
+        }
+        while (i < n1) {
+          array[k] = L[i];
+          i++;
+          k++;
+        }
+
+        while (j < n2) {
+          array[k] = M[j];
+          j++;
+          k++;
+        }
+  }
+  public static void mergeSort(int array[], int left, int right) {
+    if (left < right) {
+
+      // m is the point where the array is divided into two sub arrays
+      int mid = (left + right) / 2;
+
+      // recursive call to each sub arrays
+      mergeSort(array, left, mid);
+      mergeSort(array, mid + 1, right);
+
+      // Merge the sorted sub arrays
+      merge(array, left, mid, right);
+    }
+  }   
+}
