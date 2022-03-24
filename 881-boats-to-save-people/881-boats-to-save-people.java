d8fd68dedf792e84ca0af2f993b9c75c272a2335
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-03-24 11:56:57 +0530

    Time: 20 ms (61.45%), Space: 66 MB (14.62%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int ans = 0;
+        int start = 0;
+        int end = people.length-1;
+        while(start<=end) {
+            if(people[end] > limit) {
+                return -1;
+            }
+            if(start==end) {
+                ans++;
+                break;
+            }
+            if(people[start] + people[end] > limit) {
+                end--;
+                ans++;
+            }
+            if(people[start] + people[end] <= limit) {
+                start++;
+                end--;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
