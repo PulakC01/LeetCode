44a076d8295ff0b93b05145afe3b61a2bd2a2359
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-04-09 10:14:39 +0530

    Time: 19 ms (41.88%), Space: 50.3 MB (65.82%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        ArrayList<Integer> l = new ArrayList<>();
+        for(int i : nums) {
+            Integer count = m.get(i);
+            if (count == null)
+                m.put(i,1);
+            else
+                m.put(i, count + 1); 
+        }
+        int x = 0;
+        m.entrySet().stream().sorted((k1, k2) -> -k1.getValue().compareTo(k2.getValue()))
+            .forEach(z -> l.add(z.getKey()));
+        
+        int ans[] = new int[k];
+        for(int i=0;i<k;i++) 
+            ans[i] = l.get(i);
+       
+        return ans;
+    }
+}
