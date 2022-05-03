295ca01685ba6d730fdadbd110d5656b1b641fa4
Pulak Chowdhury <pulak.chowdhury0@gmail.com>
2022-05-03 19:43:26 +0530

    Time: 60 ms (40.95%), Space: 18.4 MB (64.48%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.cpp
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.cpp
@@ -10,7 +10,7 @@ public:
                 count=1;
             }
             else if(maxLen==sideLen)
-                ++count;
+                count++;
         }
         
         return count;
